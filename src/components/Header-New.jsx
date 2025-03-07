19aead00e9428ac27d139d3e75c6380d9c97c6fb
Chandra Obula Reddy <111179930+chandraobula@users.noreply.github.com>
2025-03-07 15:23:16 +0530

    Update Header-New.jsx for amplify issue

--- src/components/Header-New.jsx
+++ src/components/Header-New.jsx
@@ -1,3 +1,147 @@
+// import React, { useEffect, useState } from "react";
+// import { useLocation, useNavigate } from "react-router-dom";
+// import { useDispatch, useSelector } from "react-redux";
+// import { logout } from "../redux/reducers/LoginAuth";
+// import Button from "./Buttons";
+// import { apiService } from "../assets/api/apiService";
+// import "../styles/variables.css";
+// import "./Header-New.css";
+// import Logo from "../assets/images/logo.svg";
+// import ProfileIcon from "../assets/images/Profile-start.svg";
+
+// const Header = () => {
+//   const location = useLocation();
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+//   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+//   //const isLoggedIn = true;
+//   console.log("Login check",isLoggedIn);
+//   const { user, profileUpdated } = useSelector((state) => state.auth);
+//   const [currentProfileImage, setCurrentProfileImage] = useState(ProfileIcon);
+//   const[confirmLogout,setConfirmLogout] = useState(false);
+
+//   useEffect(() => {
+//     setCurrentProfileImage(user?.profile_picture_url || ProfileIcon);
+//   }, [user.profile_picture_url, profileUpdated]);
+
+//   const userId = user?.user_id;
+//   const userRole = user?.role;
+
+//   const handleLogin = () => {
+//     navigate("/Login");
+//   };
+
+//   const handleLogout = async () => {
+//     try {
+//       await apiService.logout(userId);
+//       dispatch(logout());
+//       navigate("/");
+//     } catch (error) {
+//       navigate("/server-error");
+//       console.error("Error logging out:", error);
+//     }
+//   };
+
+//   const handleProfileClick = () => {
+//     if (userId) {
+//       if (userRole === "Admin") {
+//         if (location.pathname.startsWith("/admin/users")) {
+//           navigate(`/admin/users/profile/${userId}`);
+//         } else {
+//           navigate(`/admin/users`);
+//         }
+//        } else if (userRole === "User") {
+//         navigate(`/users/profile/${userId}`);
+//       }
+//     } else {
+//       console.warn("User ID not found in Redux state");
+//     }
+//   };
+
+// const handleCancelLogout = (e)=>{
+//   e.stopPropagation();
+//   setConfirmLogout(false);
+// }
+// const initialLogout =()=>{
+//   setConfirmLogout(true);
+
+// }
+// const finalLogout =()=>{
+//   handleLogout();
+// setConfirmLogout(false);
+// }
+//   return (
+//     <div
+//       className="Navbar-Header"
+//       style={{
+//         width: location.pathname.startsWith("/article") ? "100%" : "100%",
+//       }}
+//     >
+//       <div className="Navbar-Header-Items">
+//         <a href="https://www.infersol.com/" target="_blank" rel="noreferrer">
+//           <img className="Search-nav-logo" src={Logo} alt="Infer logo" />
+//         </a>
+//         <div className="line-between">
+//           <span className="line-in-between"></span>
+//         </div>
+//         <div className="logo-descreption">
+//           <p>Information For Excellence in Research</p>
+//           <span>using Artificial Intelligence</span>
+//         </div>
+//       </div>
+
+//       <section className="Search-nav-login">
+       
+//          {isLoggedIn ? (
+//           <>
+//             <div
+//               onClick={handleProfileClick}
+//               style={{ cursor: "pointer", height: "25px" }}
+//             >
+//               <img
+//                 src={currentProfileImage}
+//                 style={{ width: "25px", height: "25px", borderRadius: "50%" }}
+//                 alt="Profile"
+//                 className="profile-icon"
+//               />
+//             </div>
+//             <span  onClick={initialLogout}>Logout here</span>
+//             <div>
+//               <h5  onClick={initialLogout}>logout here</h5>
+//             </div>
+//             <Button
+//               text="Logout"
+//               className="logout-btn"
+//               //onClick={handleLogout}
+//               onClick={initialLogout}
+//               style={{ display: "block" ,background:"black",border:"1px solid black"}}
+//             />
+//           </>
+//         ) : (
+//            <span  onClick={handleLogin}>Login here</span>
+//             <div>
+//               <h5  onClick={handleLogin}>login here</h5>
+//             </div>
+//           <Button text="Login" className="login-btn" onClick={handleLogin} style={{ display: "block",background:"black",border:"1px solid black" }}/>
+//         )}
+//         {confirmLogout &&(
+//           <div className="confirmlogout-overlay">
+//             <div className="confirmlogout-popup">
+//               <p>Are you sure to logout?</p>
+//               <div className="confirmlogout-buttons">
+
+//               <button className="cancel-logout" onClick={handleCancelLogout}>cancel</button>
+//               <button className="confirm-logout" onClick={finalLogout}>Logout</button>
+//               </div>
+//             </div>
+//           </div>
+//         ) } 
+//       </section>
+//     </div>
+//   );
+// };
+
+// export default Header;
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -14,22 +158,19 @@ const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  //const isLoggedIn = true;
-  console.log("Login check",isLoggedIn);
+  console.log("Login check", isLoggedIn);
   const { user, profileUpdated } = useSelector((state) => state.auth);
   const [currentProfileImage, setCurrentProfileImage] = useState(ProfileIcon);
-  const[confirmLogout,setConfirmLogout] = useState(false);
+  const [confirmLogout, setConfirmLogout] = useState(false);
 
   useEffect(() => {
     setCurrentProfileImage(user?.profile_picture_url || ProfileIcon);
-  }, [user.profile_picture_url, profileUpdated]);
+  }, [user?.profile_picture_url, profileUpdated]);
 
   const userId = user?.user_id;
   const userRole = user?.role;
 
-  const handleLogin = () => {
-    navigate("/Login");
-  };
+  const handleLogin = () => navigate("/Login");
 
   const handleLogout = async () => {
     try {
@@ -50,7 +191,7 @@ const Header = () => {
         } else {
           navigate(`/admin/users`);
         }
-       } else if (userRole === "User") {
+      } else if (userRole === "User") {
         navigate(`/users/profile/${userId}`);
       }
     } else {
@@ -58,24 +199,22 @@ const Header = () => {
     }
   };
 
-const handleCancelLogout = (e)=>{
-  e.stopPropagation();
-  setConfirmLogout(false);
-}
-const initialLogout =()=>{
-  setConfirmLogout(true);
+  const handleCancelLogout = (e) => {
+    e.stopPropagation();
+    setConfirmLogout(false);
+  };
 
-}
-const finalLogout =()=>{
-  handleLogout();
-setConfirmLogout(false);
-}
+  const initialLogout = () => setConfirmLogout(true);
+
+  const finalLogout = () => {
+    handleLogout();
+    setConfirmLogout(false);
+  };
+
   return (
     <div
       className="Navbar-Header"
-      style={{
-        width: location.pathname.startsWith("/article") ? "100%" : "100%",
-      }}
+      style={{ width: location.pathname.startsWith("/article") ? "100%" : "100%" }}
     >
       <div className="Navbar-Header-Items">
         <a href="https://www.infersol.com/" target="_blank" rel="noreferrer">
@@ -91,13 +230,9 @@ setConfirmLogout(false);
       </div>
 
       <section className="Search-nav-login">
-       
-         {isLoggedIn ? (
+        {isLoggedIn ? (
           <>
-            <div
-              onClick={handleProfileClick}
-              style={{ cursor: "pointer", height: "25px" }}
-            >
+            <div onClick={handleProfileClick} style={{ cursor: "pointer", height: "25px" }}>
               <img
                 src={currentProfileImage}
                 style={{ width: "25px", height: "25px", borderRadius: "50%" }}
@@ -105,37 +240,46 @@ setConfirmLogout(false);
                 className="profile-icon"
               />
             </div>
-            <span  onClick={initialLogout}>Logout here</span>
+            <span onClick={initialLogout}>Logout here</span>
             <div>
-              <h5  onClick={initialLogout}>logout here</h5>
+              <h5 onClick={initialLogout}>logout here</h5>
             </div>
             <Button
               text="Logout"
               className="logout-btn"
-              //onClick={handleLogout}
               onClick={initialLogout}
-              style={{ display: "block" ,background:"black",border:"1px solid black"}}
+              style={{ display: "block", background: "black", border: "1px solid black" }}
             />
           </>
         ) : (
-           <span  onClick={handleLogin}>Login here</span>
+          <>
+            <span onClick={handleLogin}>Login here</span>
             <div>
-              <h5  onClick={handleLogin}>login here</h5>
+              <h5 onClick={handleLogin}>login here</h5>
             </div>
-          <Button text="Login" className="login-btn" onClick={handleLogin} style={{ display: "block",background:"black",border:"1px solid black" }}/>
+            <Button
+              text="Login"
+              className="login-btn"
+              onClick={handleLogin}
+              style={{ display: "block", background: "black", border: "1px solid black" }}
+            />
+          </>
         )}
-        {confirmLogout &&(
+        {confirmLogout && (
           <div className="confirmlogout-overlay">
             <div className="confirmlogout-popup">
               <p>Are you sure to logout?</p>
               <div className="confirmlogout-buttons">
-
-              <button className="cancel-logout" onClick={handleCancelLogout}>cancel</button>
-              <button className="confirm-logout" onClick={finalLogout}>Logout</button>
+                <button className="cancel-logout" onClick={handleCancelLogout}>
+                  cancel
+                </button>
+                <button className="confirm-logout" onClick={finalLogout}>
+                  Logout
+                </button>
               </div>
             </div>
           </div>
-        ) } 
+        )}
       </section>
     </div>
   );
