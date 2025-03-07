dc1fd50d78f480577865ce50533f6ffd1256bddf
Chandra Obula Reddy <111179930+chandraobula@users.noreply.github.com>
2025-03-07 14:42:17 +0530

    Update Buttons.jsx for amplify

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "../styles/variables.css";
 import "./Button.css"; // Import the CSS specific to this button
 
-const Button = ({ text, onClick, className, loading }) => {
+const Button = ({ text, onClick, className, loading,style }) => {
   return (
     <button
       className={`custom-button ${className}`}
       onClick={onClick}
       disabled={loading}
+      style={{ display: 'inline-block', ...style }}
     >
       {loading ? <div class="searchbar-loader"></div> : text}
     </button>
